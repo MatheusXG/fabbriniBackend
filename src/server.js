770bf6f522977b625d9f985ffaf3a3ae217ab9e2
Mx <matheusgodois0@gmail.com>
2021-08-20 09:55:32 -0300

    atualizando variaveis

--- src/server.js
+++ src/server.js
@@ -21,6 +21,6 @@ app.use(express.static(__dirname));
 app.use(express.json());
 
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`===============Servidor está rondando===============`);
 });
